da1123af6373f8d3a03be697700d24ca2a43edae
ilyailyaovch <ilyailyaovch@gmail.com>
2026-07-11 11:06:36 +0300

    Initial Commit

---
+++ TripPalette/TripPaletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripPaletteApp.swift
+//  TripPalette
+//
+//  Created by Ilya Ovchinnikov on 11.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct TripPaletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
